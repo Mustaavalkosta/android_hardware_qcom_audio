d1f85a216ec3d9707f2800576cce1d4dab88a008
Nicholas Flintham <nick@flinny.org>
2012-12-05 14:08:55 +0000

    non caf audio bringup for JB4.2

--- msm7x30/Android.mk
+++ msm7x30/Android.mk
@@ -83,39 +83,6 @@ LOCAL_C_INCLUDES += system/core/include
 
 include $(BUILD_SHARED_LIBRARY)
 
-# The audio policy is implemented on top of legacy policy code
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
-    AudioPolicyManager.cpp \
-    audio_policy_hal.cpp
-
-LOCAL_SHARED_LIBRARIES := \
-    libcutils \
-    libutils \
-    libmedia
-
-LOCAL_STATIC_LIBRARIES := \
-    libaudiohw_legacy \
-    libmedia_helper \
-    libaudiopolicy_legacy
-
-LOCAL_MODULE := audio_policy.$(TARGET_BOARD_PLATFORM)
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE_TAGS := optional
-
-ifeq ($(BOARD_HAVE_BLUETOOTH),true)
-    LOCAL_CFLAGS += -DWITH_A2DP
-endif
-
-ifeq ($(BOARD_USES_QCOM_AUDIO_LPA),true)
-    LOCAL_CFLAGS += -DQCOM_TUNNEL_LPA_ENABLED
-endif
-
-LOCAL_C_INCLUDES := hardware/libhardware_legacy/audio
-
-include $(BUILD_SHARED_LIBRARY)
-
 # Load audio_policy.conf to system/etc/
 include $(CLEAR_VARS)
 LOCAL_MODULE       := audio_policy.conf
